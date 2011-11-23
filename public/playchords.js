487df2152c124c4eda262d47a836d6e819cb6b68
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-11-23 01:09:01 +0100

    play A minor

--- public/playchords.js
+++ public/playchords.js
@@ -9,7 +9,7 @@ try {
 }
 
 var ready = function (sounds) {
-  var chord = [sounds.e0, sounds.e1, sounds.e2, sounds.e3, sounds.e4];
+  var chord = [sounds.g5, sounds.d2, sounds.a0, sounds.e0];
   // allow the user to see the interface, indicating that they can play now
   document.body.className = 'loaded';
   document.querySelector('button').addEventListener('click', function () {
@@ -17,18 +17,14 @@ var ready = function (sounds) {
   });
 };
 
-var frets = (function () {
-  var hash = {};
-  var permalinks = ["e0-2", "e1-1", "e2-2", "e3-2", "e4-2", "e5-2", "e6-2", "e7-2", "e8-2", "e9-2", "e10-2", "e11-wav"];
+var notes = {
+  g5: '/fronx/g5-1/audio',
+  d2: '/fronx/d2-1/audio',
+  a0: '/fronx/a0-1/audio',
+  e0: '/fronx/e0-2/audio'
+};
 
-  permalinks.forEach( function (permalink, i) {
-    hash['e' + i] = '/fronx/' + permalink + '/audio';
-  });
-
-  return hash;
-}());
-
-var sounds = loadSounds(frets, ready);
+var sounds = loadSounds(notes, ready);
 
 // requires context to exist in global scope
 var playSound =  function (buffer, at) {
@@ -50,4 +46,15 @@ var playChord = function (chord) {
   });
 };
 
+var chordKey = window.location.hash;
+
+var chordKeyToToneKeys = function(chordKey) {
+  return chordKey           // #e0b1g2D2A0Ex--A_minor
+    .replace('#', '')       // e0b1g2D2A0Ex--A_minor
+    .split('--')[0]         // e0b1g2D2A0Ex
+    .split(/([a-zA-Z]\d+)/) // ["", "e0", "", "b1", "", "g2", "", "D2", "", "A0", "Ex"]
+    .filter(function(item){ // [e0", b1", g2", D2", A0"]
+      return item.match(/([a-zA-Z]\d+)/);
+    });
+};
 
